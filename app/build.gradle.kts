489ebd51a9acd65c91142a02898b9ea38c7fa890
obkwon <obin0125@gmail.com>
2026-01-10 14:29:04 +0900

    이미지 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.game.cozyfly"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
