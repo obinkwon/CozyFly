fc837c2e5dd39fd25631c228b291c76e7d1bb226
obkwon <obin0125@gmail.com>
2026-04-02 21:50:04 +0900

    상점 화면 수정 및 스킨 추가

--- app/src/main/java/com/game/cozyfly/view/ShopView.kt
+++ app/src/main/java/com/game/cozyfly/view/ShopView.kt
@@ -26,27 +26,33 @@ class ShopView(
     private var showing = false
     private var centerX = 0f // 중앙 X좌표 값
     private var centerY = 0f // 중앙 Y좌표 값
-    private var startX = 0f
-    private val swipeThreshold = 100 // 스와이프 판정 거리(px)
     private val backBtn: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.back)
     private val characterImgs: Array<Array<Bitmap>> = arrayOf(
         arrayOf(
             BitmapFactory.decodeResource(resources, R.drawable.fly1),
-            BitmapFactory.decodeResource(resources, R.drawable.fly2)
+            BitmapFactory.decodeResource(resources, R.drawable.fly2),
+                    BitmapFactory.decodeResource(resources, R.drawable.fly_name)
         ),
         arrayOf(
             BitmapFactory.decodeResource(resources, R.drawable.sprite1),
-            BitmapFactory.decodeResource(resources, R.drawable.sprite2)
+            BitmapFactory.decodeResource(resources, R.drawable.sprite2),
+            BitmapFactory.decodeResource(resources, R.drawable.sprite_name)
         )
     )
+    private val leftBtn: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.left)
+    private val rightBtn: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.right)
     // 현재 선택된 스킨
     private var currentSkin = characterImgs[0][0]
+    private var currentName = characterImgs[0][2]
     private var isCycling = false
     private var currentCharacterIndex = 0 // 어떤 캐릭터
     private var currentImageIndex = 0     // 그 캐릭터의 몇 번째 이미지
 
     private val backBtnRect = RectF()
     private val characterRect = RectF()
+    private val characterNameRect = RectF()
+    private val leftBtnRect = RectF()
+    private val rightBtnRect = RectF()
 
     // 화면 표시
     fun showView() {
@@ -81,6 +87,18 @@ class ShopView(
         // 캐릭터 버튼
         characterRect.set(ButtonUtil.getButtonSize(centerX, centerY, SizeConstants.PLAYER_BIG_WIDTH, SizeConstants.PLAYER_BIG_HEIGHT))
         canvas.drawBitmap(currentSkin, null, characterRect, null)
+
+        // 왼쪽 방향표 버튼
+        leftBtnRect.set(ButtonUtil.getButtonSize(100f, centerY, SizeConstants.SETTING_ICON_WIDTH, SizeConstants.SETTING_ICON_HEIGHT))
+        canvas.drawBitmap(leftBtn, null, leftBtnRect, null)
+
+        // 오른쪽 방향표 버튼
+        rightBtnRect.set(ButtonUtil.getButtonSize(width - 100f, centerY, SizeConstants.SETTING_ICON_WIDTH, SizeConstants.SETTING_ICON_HEIGHT))
+        canvas.drawBitmap(rightBtn, null, rightBtnRect, null)
+
+        // 현재 캐릭터 이름
+        characterNameRect.set(ButtonUtil.getButtonSize(centerX, characterRect.bottom + 300f, SizeConstants.START_BTN_WIDTH, SizeConstants.SETTING_BTN_HEIGHT))
+        canvas.drawBitmap(currentName, null, characterNameRect, null)
     }
 
     // 터치 이벤트
@@ -88,20 +106,8 @@ class ShopView(
     override fun onTouchEvent(event: MotionEvent): Boolean {
         if (!showing || gameConfig.viewState != ViewState.SHOP) return false
 
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                startX = event.x
-            }
-
-            MotionEvent.ACTION_UP -> {
-                val diffX = event.x - startX
-
-                if (kotlin.math.abs(diffX) > swipeThreshold) {
-                    handleSwipe(diffX)
-                } else {
-                    handleShopTouch(event)
-                }
-            }
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            handleShopTouch(event)
         }
 
         return true
@@ -112,33 +118,27 @@ class ShopView(
         when {
             backBtnRect.contains(event.x, event.y) -> hideView()
             characterRect.contains(event.x, event.y) -> handleCharacterTouch()
+            leftBtnRect.contains(event.x, event.y) -> handleCharacterChange("L")
+            rightBtnRect.contains(event.x, event.y) -> handleCharacterChange("R")
         }
     }
 
     // 캐릭터 변경
-    private fun handleSwipe(diffX: Float) {
+    private fun handleCharacterChange(direction: String) {
         if (characterImgs.size <= 1) return
 
-        // 자동 애니메이션 중이면 정지
-        if (isCycling) {
-            isCycling = false
-            removeCallbacks(runnable)
+        isCycling = false
+        // 왼쪽 클릭
+        if (direction == "L") {
+            currentCharacterIndex = (currentCharacterIndex + 1) % characterImgs.size
+        }
+        // 오른쪽 클릭
+        else {
+            currentCharacterIndex = (currentCharacterIndex - 1 + characterImgs.size) % characterImgs.size
         }
 
-        if (diffX > 0) {
-            // 👉 오른쪽 스와이프 → 이전 캐릭터
-            currentCharacterIndex =
-                (currentCharacterIndex - 1 + characterImgs.size) % characterImgs.size
-        } else {
-            // 👉 왼쪽 스와이프 → 다음 캐릭터
-            currentCharacterIndex =
-                (currentCharacterIndex + 1) % characterImgs.size
-        }
-
-        // 👉 캐릭터 바뀌면 이미지 초기화
-        currentImageIndex = 0
-
-        currentSkin = characterImgs[currentCharacterIndex][currentImageIndex]
+        currentSkin = characterImgs[currentCharacterIndex][0]
+        currentName = characterImgs[currentCharacterIndex][2]
         invalidate()
     }
 
@@ -151,7 +151,7 @@ class ShopView(
 
             if (currentImgs.isEmpty()) return
 
-            currentImageIndex = (currentImageIndex + 1) % currentImgs.size
+            currentImageIndex = if (currentImageIndex == 0) 1 else 0
             currentSkin = currentImgs[currentImageIndex]
 
             invalidate()
